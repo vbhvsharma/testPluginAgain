35a3605697d11be9603c49b5ca615f2e84ab2d32
klassify <klassify@princes-mac-mini.local>
2018-06-15 12:52:05 +0530

    save A 3

--- testPluginAgain/ViewController.swift
+++ testPluginAgain/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var pluginData : String ="plugin.aps"
+    var pluginData : String = "plugin.aps"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
